461622c80e42e87f4c6c1fc9d2cd59036e0dd1ca
mystars100826 <mystars100826@gmail.com>
2026-02-21 02:00:19 -0500

    Remove required constraint on position and company fields

--- models/reference.js
+++ models/reference.js
@@ -6,9 +6,7 @@ const ReferenceSchema = new Schema(
   {
     firstname: { type: String, required: true },
     lastname: { type: String, required: true },
-    email: { type: String, required: true },
-    position: { type: String, required: true },
-    company: { type: String, required: true }
+    email: { type: String, required: true }
   },
   { timestamps: true }
 );
